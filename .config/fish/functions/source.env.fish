ca41bc499cea80cb63db9c5425dda840f6a78024
KruASe76 <kruanse@ya.ru>
2024-11-10 00:21:13 +0300

    source.env function

---
+++ .config/fish/functions/source.env.fish
@@ -0,0 +1,21 @@
+function source.env
+    if test (count $argv) -eq 0
+        set -f env_file ".env"
+    else
+        set -f env_file $argv[1]
+    end
+
+    if test -d $env_file
+        set env_file "$env_file/.env"
+    end
+
+    if ! test -f $env_file
+        echo "File $env_file does not exist" >2
+    end
+
+    for line in (cat $env_file)
+        if ! test -z $line
+            set -gx (string split -m 1 "=" $line)
+        end
+    end
+end
